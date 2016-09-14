e55f48bfdb889ac784cb779b6d6c22d651ec3f57
looooo <sppedflyer@gmail.com>
2016-09-14 14:05:35 +0200

    bla

--- FreeCAD/build_tmp.sh
+++ FreeCAD/build_tmp.sh
@@ -1,7 +1,10 @@
-PREFIX="~/anaconda/envs/fc_2_build"
+PREFIX="/home/lorenz/miniconda2/envs/fc_2"
 cmake -DCMAKE_BUILD_TYPE=Debug \
       -DNETGENDATA=$PREFIX/include/netgen/libsrc \
       -DNETGEN_INCLUDEDIR=$PREFIX/include/netgen/nglib \
       -DBUILD_FEM_NETGEN=YES \
-      -DOCC_MANUAL_PATH=$PREFIX \
+      -DOCC_INCLUDE_DIR=$PREFIX/include/opencascade \
+      -DOCC_LIBRARY_DIR=$PREFIX/lib \
+      -DOCC_LIBRARIES=$PREFIX/lib CACHE PATH \
+      -DOCC_OCAF_LIBRARIES=$PREFIX/lib CACHE PATH \
       ..

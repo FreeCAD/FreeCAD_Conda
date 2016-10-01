694e9f5bc368ae1e903962a1b3572b22c7dbc623
builder <sppedflyer@gmail.com>
2016-10-02 00:34:46 +0200

    bla

--- shiboken/build.sh
+++ shiboken/build.sh
@@ -4,7 +4,6 @@ mkdir build
 cd build
 cmake \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_RPATH=$LD_RUN_PATH \
     -DQT_QMAKE_EXECUTABLE=$PREFIX/bin/qmake \
     -DPYTHON_EXECUTABLE=$PYTHON \

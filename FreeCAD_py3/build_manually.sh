fe418267a14ed208c10c7f6986d88b287c41b729
looooo <sppedflyer@gmail.com>
2016-10-06 07:56:16 +0200

    freecad python3 build manually

--- FreeCAD_py3/build_manually.sh
+++ FreeCAD_py3/build_manually.sh
@@ -1,21 +1,17 @@
-set > variables.txt #write all enviroment stuff into this
-
-mkdir build
-cd build
 
 VERBOSE=1 cmake -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DNETGENDATA=$PREFIX/include/netgen/libsrc \
-      -DNETGEN_INCLUDEDIR=$PREFIX/include/netgen/nglib \
-      -DNGLIB_INCLUDE_DIR=$PREFIX/include/netgen/nglib \
-      -DOCC_INCLUDE_DIR=$PREFIX/include/opencascade \
-      -DOCC_LIBRARY_DIR=$PREFIX/lib \
-      -DOCC_LIBRARIES=$PREFIX/lib CACHE PATH \
-      -DOCC_OCAF_LIBRARIES=$PREFIX/lib CACHE PATH \
-      -DSWIG_DIR=$PREFIX/share/swig/3.0.8 \
-      -DSWIG_EXECUTABLE=$PREFIX/bin/swig \
-      -DPYTHON_EXECUTABLE=$PYTHON \
+      -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX \
+      -DCMAKE_PREFIX_PATH=$CONDA_PREFIX \
+      -DNETGENDATA=$CONDA_PREFIX/include/netgen/libsrc \
+      -DNETGEN_INCLUDEDIR=$CONDA_PREFIX/include/netgen/nglib \
+      -DNGLIB_INCLUDE_DIR=$CONDA_PREFIX/include/netgen/nglib \
+      -DOCC_INCLUDE_DIR=$CONDA_PREFIX/include/opencascade \
+      -DOCC_LIBRARY_DIR=$CONDA_PREFIX/lib \
+      -DOCC_LIBRARIES=$CONDA_PREFIX/lib CACHE PATH \
+      -DOCC_OCAF_LIBRARIES=$CONDA_PREFIX/lib CACHE PATH \
+      -DSWIG_DIR=$CONDA_PREFIX/share/swig/3.0.8 \
+      -DSWIG_EXECUTABLE=$CONDA_PREFIX/bin/swig \
+      -DPYTHON_EXECUTABLE=$CONDA_PREFIX/bin/python \
       -DBUILD_FEM_NETGEN=YES \
       -DBUILD_ARCH=YES \
       -DBUILD_ASSEMBLY=NO \
@@ -50,10 +46,3 @@ VERBOSE=1 cmake -DCMAKE_BUILD_TYPE=Release \
       -DBUILD_TEST=YES \
       -DBUILD_VR=NO \
       -DBUILD_WEB=YES ..
-
-make -j3 2>&1 | tee output.txt
-make install
-# mkdir $PREFIX/bin -p
-# ln -s ../lib/freecad/bin/FreeCAD $PREFIX/bin/freecad
-# ln -s ../lib/freecad/bin/FreeCADCmd $PREFIX/bin/FreeCADCmd
-rm ${PREFIX}/doc -r     # smaller size of package!

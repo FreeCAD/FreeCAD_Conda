b344afc258f32bc50cc289ce4fe48a4dbf5c833a
looooo <sppedflyer@gmail.com>
2017-01-24 12:02:48 +0100

    update netgen

--- .netgen/build.sh
+++ .netgen/build.sh
@@ -6,20 +6,24 @@ export PYTHONPATH=$SP_DIR
 echo "set PYTHONPATH to: $PYTHONPATH"
 echo "------------------------------------------------"
 
+mkdir build
+cd build 
+
 cmake -DCMAKE_BUILD_TYPE=Release \
-	  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DINSTALL_DIR=$PREFIX \
       -DUSE_OCC=ON \
-      -DUSE_PYTHON=OFF \
-      -DUSE_GUI=ON \
+      -DUSE_PYTHON=ON \
+      -DUSE_GUI=OFF \
       -DOCC_INCLUDE_DIR=$PREFIX/include/opencascade \
-      -OCC_LIBRARY_DIR=$PREFIX/lib \
-      .
-make -j4 2>&1 | tee output.txt
+      -DOCC_LIBRARY_DIR=$PREFIX/lib \
+      -DPYBIND_INCLUDE_DIR=$PREFIX/include/pybind11 \
+      ..
+make -j5 2>&1 | tee output.txt
 make install 
 
 mkdir ${PREFIX}/include/netgen
 
-
-rsync -avm --include='*.h*' -f 'hide,! */' . ${PREFIX}/include/netgen
-# find . -name "*.h*" -type f -exec cp {} ${PREFIX}/include/netgen \;
+rsync -avm --include='*.h*' -f 'hide,! */' .. ${PREFIX}/include/netgen
+cp ${PREFIX}/include/netgen/build/config.h ${PREFIX}/include/netgen/mydefs.hpp
+rm ${PREFIX}/include/netgen/build -r

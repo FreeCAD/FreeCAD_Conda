bfe605cea225a4ff2292396bbfeae1232b5e2c1b
looooo <sppedflyer@gmail.com>
2017-02-13 18:46:13 +0100

    update netgen

--- netgen/build.sh
+++ netgen/build.sh
@@ -19,11 +19,11 @@ cmake -DCMAKE_BUILD_TYPE=Release \
       -DOCC_LIBRARY_DIR=$PREFIX/lib \
       -DPYBIND_INCLUDE_DIR=$PREFIX/include/pybind11 \
       ..
+
 make -j5 2>&1 | tee output.txt
 make install 
 
 mkdir ${PREFIX}/include/netgen
 
 rsync -avm --include='*.h*' -f 'hide,! */' .. ${PREFIX}/include/netgen
-cp ${PREFIX}/include/netgen/build/config.h ${PREFIX}/include/netgen/mydefs.hpp
 rm ${PREFIX}/include/netgen/build -r

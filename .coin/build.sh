aacb69ee96718b22b3a34894348d47ee1bae6e18
looooo <sppedflyer@gmail.com>
2017-03-11 19:45:57 +0100

    delete .coin

--- .coin/build.sh
+++
@@ -1,6 +0,0 @@
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_PREFIX_PATH=$PREFIX .
-make install -j4
-
-cp -r ${RECIPE_DIR}/Coin ${PREFIX}/share/
-cp ${SRC_DIR}/bin/coin-config ${PREFIX}/bin/

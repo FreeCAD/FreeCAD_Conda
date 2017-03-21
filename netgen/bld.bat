ad94fe5aa09741e6249dd5dd332c8aaae47b02bd
looooo <sppedflyer@gmail.com>
2017-03-21 06:41:46 +0100

    add lib-med bat file

--- netgen/bld.bat
+++ netgen/bld.bat
@@ -4,21 +4,20 @@ cd build
 cmake .. -G "Ninja" ^
     -DCMAKE_BUILD_TYPE="Release" ^
     -DINSTALL_DIR_LAYOUT="Unix" ^
-    -DCMAKE_PREFIX_PATH:FILEPATH="%PREFIX%" ^
-    -DCMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%" ^
-    -DINSTALL_DIR_FILEPATH="%LIBRARY_PREFIX%" ^
-    -DINCDIR:FILEPATH=%LIBRARY_PREFIX%/include/netgen ^
-    -DOCC_INCLUDE_DIR:FILEPATH=%LIBRARY_PREFIX%/include/opencascade ^
-    -DOCC_LIBRARY_DIR:FILEPATH=%LIBRARY_PREFIX%/lib ^
+    -DCMAKE_PREFIX_PATH=%PREFIX% ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DINSTALL_DIR=%LIBRARY_PREFIX% ^
+    -DOCC_INCLUDE_DIR=%LIBRARY_PREFIX%\include\opencascade ^
+    -DOCC_LIBRARY_DIR=%LIBRARY_PREFIX%\lib ^
     -DUSE_OCC=ON ^
     -DUSE_PYTHON=OFF ^
     -DUSE_GUI=OFF ^
-    -DUSE_SUPERBUILD=OFF ^
+    -DUSE_SUPERBUILD=OFF
 
 if errorlevel 1 exit 1
 
 ninja install
 if errorlevel 1 exit 1
 
-mkdir %LIBRARY_PREFIX%/include/netgen -p
-Copy %SRC_DIR%\build\config.h %PREFIX%\include\netgen\mydefs.hpp
+mkdir %LIBRARY_PREFIX%\include\netgen -p
+Copy %SRC_DIR%\build\config.h %LIBRARY_PREFIX%\include\netgen\mydefs.hpp

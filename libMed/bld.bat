612197ea3d5d45139879a20afc5b93435b32a5b5
looooo <sppedflyer@gmail.com>
2017-03-22 10:15:23 +0100

    make libmed compile on windows

--- libMed/bld.bat
+++ libMed/bld.bat
@@ -1,8 +1,10 @@
 cmake . -G "Ninja" ^
-    -DINSTALL_DIR_LAYOUT="Unix" ^
     -DCMAKE_PREFIX_PATH=%PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-    -DHDF5_LIBRARIES=%LIBRARY_PREFIX%\lib\libhdf5.lib
+    -DHDF5_LIBRARIES=%LIBRARY_PREFIX%\lib\libhdf5.lib ^
+    -DHDF5_ROOT_DIR=%LIBRARY_PREFIX%
+
+rem MEDFILE_BUILD_PYTHON -> swig, maybe this is a nice option
 
 if errorlevel 1 exit 1
 ninja install

ac1db0601fd95713e1c3255ae393a73dd685980f
looooo <sppedflyer@gmail.com>
2017-03-22 16:10:51 +0100

    add freecad bld.bat

--- libMed/bld.bat
+++ libMed/bld.bat
@@ -2,10 +2,13 @@ cmake . -G "Ninja" ^
     -DCMAKE_PREFIX_PATH=%PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DHDF5_LIBRARIES=%LIBRARY_PREFIX%\lib\libhdf5.lib ^
-    -DHDF5_ROOT_DIR=%LIBRARY_PREFIX%
+    -DHDF5_ROOT_DIR=%LIBRARY_PREFIX% ^
+    -DMEDFILE_INSTALL_DOC=OFF
 
 rem MEDFILE_BUILD_PYTHON -> swig, maybe this is a nice option
 
 if errorlevel 1 exit 1
 ninja install
 if errorlevel 1 exit 1
+
+rmdir /s /q %LIBRARY_PREFIX%\share
--- libMed/build.sh
+++ libMed/build.sh
@@ -1,5 +1,6 @@
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DINSTALL_DIR=$PREFIX \
+      -DMEDFILE_INSTALL_DOC=OFF \
       .
 make -j4
 make install

fa05f99b8cfd3a2d6c32800c59c17b5bf7ae345a
looooo <sppedflyer@gmail.com>
2017-03-26 07:11:51 +0200

    delete pyside hack

--- FreeCAD/win_path.py
+++
@@ -1,9 +0,0 @@
-import sys, fileinput
-
-path = sys.argv[1]
-
-with fileinput.FileInput(path, inplace=True) as file:
-    for line in file:
-        line = line.replace("\\", "/")
-        line = line.replace("\L", "/L")
-        print(line, end="")

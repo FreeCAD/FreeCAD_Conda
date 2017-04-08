ea0abd0dad9183ad012cbaa42c683931561b0c42
looooo <sppedflyer@gmail.com>
2017-04-08 07:24:27 +0200

    calculix: win

---
+++ calculix/bld.bat
@@ -0,0 +1,15 @@
+
+cd ARPACK
+sh ./fixhome.sh
+make lib
+
+cd ..
+cd SPOOLES.2.2
+make lib -f makefile_MT
+
+cd ..
+cd CalculiX/ccx_2.12/src;
+make -f Makefile_MT
+
+cp ccx_2.12_MT %LIBRARY_PREFIX%/bin/ccx
+cd $SRC_DIR

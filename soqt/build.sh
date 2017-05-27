f303365d72d483cf715f5a082193bf82bab123c1
looooo <sppedflyer@gmail.com>
2017-05-27 11:31:19 +0200

    soqt debugging

--- soqt/build.sh
+++ soqt/build.sh
@@ -6,6 +6,7 @@ cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
       -DCMAKE_BUILD_TYPE="Release" \
       -DUSE_QT5=OFF \
+      -DCOIN_INCLUDE_DIR=$PREFIX/include \
       ..
 
 make -j4 install

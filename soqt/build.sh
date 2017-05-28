974ab21e3392662fd986dd4b7631bb9bc29cb560
looooo <sppedflyer@gmail.com>
2017-05-28 14:42:53 +0200

    soqt: use .configure

--- soqt/build.sh
+++ soqt/build.sh
@@ -1,12 +1,15 @@
 mkdir -p build
 cd build
 
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
-      -DCMAKE_BUILD_TYPE="Release" \
-      -DUSE_QT5=OFF \
-      -DCOIN_INCLUDE_DIR=$PREFIX/include \
-      ..
+# cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
+#       -DCMAKE_PREFIX_PATH=$PREFIX \
+#       -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
+#       -DCMAKE_BUILD_TYPE="Release" \
+#       -DUSE_QT5=OFF \
+#       ..
+
+../configure \
+	--prefix=$PREFIX \
+	--exec_prefix=$PREFIX
 
 make -j4 install

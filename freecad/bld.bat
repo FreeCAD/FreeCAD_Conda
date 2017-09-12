89c3f17d03456621765d8b5e4e798ea43d84e9bc
looooo <sppedflyer@gmail.com>
2017-09-12 14:56:24 +0200

    add constructor recipe
    + some diff for freecad-recipe

--- freecad/bld.bat
+++ freecad/bld.bat
@@ -9,7 +9,7 @@ cmake .. -G "Ninja" ^
       -DCMAKE_INCLUDE_PATH=%LIBRARY_PREFIX%/include ^
       -DCMAKE_LIBRARY_PATH=%LIBRARY_PREFIX%/lib ^
       -DBUILD_QT5=ON ^
-      -DNETGENDATA=%LIBRARY_PREFIX%/include ^
+      -DNETGENDATA=%LIBRARY_PREFIX%/include/netgen ^
       -DNETGEN_INCLUDEDIR=%LIBRARY_PREFIX%/include/netgen ^
       -DNGLIB_INCLUDE_DIR=%LIBRARY_PREFIX%/include/nglib ^
       -DOCC_INCLUDE_DIR=%LIBRARY_PREFIX%/include/opencascade ^
@@ -20,8 +20,7 @@ cmake .. -G "Ninja" ^
       -DSWIG_DIR=%LIBRARY_PREFIX%/share/swig/3.0.8 ^
       -DSWIG_EXECUTABLE=%LIBRARY_PREFIX%/bin/swig ^
       -DPYTHON_EXECUTABLE=%PYTHON% ^
-      -DBUILD_REVERSEENGINEERING=NO ^
-      -DUSE_BOOST_PYTHON=NO
+      -DBUILD_REVERSEENGINEERING=NO
 
 
 if errorlevel 1 exit 1

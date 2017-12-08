fe0c980ec0ab1d447f3d2b50bc381eed4bfa1cd7
looooo <sppedflyer@gmail.com>
2017-12-08 19:10:14 +0100

    use bitbucket source for coin

--- coin3d/build.sh
+++ coin3d/build.sh
@@ -3,18 +3,8 @@ cd build
 
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DCOIN_LINK_LIBSIMAGE=ON \
+      -DSIMAGE_RUNTIME_LINKING=ON \
+      -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
       ..
 
 make -j${CPU_COUNT} install
-
-# Certain apps, like pivy, need coin-config. Cmake does not yet generate the coin-default.cfg
-cd ..
-mkdir build-cfg -p
-cd build-cfg
-../configure --prefix=$PREFIX --without-framework --enable-3ds-import --disable-dependency-tracking
-make coin-default.cfg
-
-mkdir $PREFIX/share/Coin/conf -p
-cp coin-default.cfg $PREFIX/share/Coin/conf/
-cp ${SRC_DIR}/bin/coin-config ${PREFIX}/bin/

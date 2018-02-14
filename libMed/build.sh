d22a695edf87501bff586be11b9e7ab1d87ecb04
looooo <sppedflyer@gmail.com>
2018-02-14 23:00:02 +0100

    smesh

--- libMed/build.sh
+++ libMed/build.sh
@@ -7,7 +7,7 @@ cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
       ..
 
-make -j-j${CPU_COUNT}
+make -j${CPU_COUNT}
 make install
 
 rm -r $PREFIX/share/doc
--- smesh/build.sh
+++ smesh/build.sh
@@ -3,13 +3,8 @@ cd build
 
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
-      -DRAISE_EXCEPTION_ON_FAILURE=ON \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
-      -DSMESH_USE_BUNDLED_BOOST=ON \
-      -DSMESH_TESTING=ON \
-      -DPTHREAD_INCLUDE_DIRS=$PREFIX/include \
-      -DPTHREAD_LIB_DIRS=$PREFIX/lib \
-      ..
+      ~/projects/SMESH
 
 make -j${CPU_COUNT} install

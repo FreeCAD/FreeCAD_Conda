8cc4a809c70cd2aa5573f68016f58cc2321a2beb
looooo <sppedflyer@gmail.com>
2018-02-15 08:10:36 +0100

    mesh + add povray

---
+++ povray/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+# export LDFLAGS="-L$PREFIX/lib"
+# export CPPFLAGS="-I$PREFIX/include"
+
+(cd unix && ./prebuild.sh)
+
+./configure \
+    --prefix="$PREFIX" \
+    --with-boost="$PREFIX" \
+    --with-boost-thread=boost_thread \
+    --without-x \
+    --without-openexr \
+    --without-libsdl \
+
+make
+make install

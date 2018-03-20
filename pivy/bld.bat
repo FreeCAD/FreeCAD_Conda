6cd12548e61e7b2d558aad8ba439ef2a80298e8c
looooo <sppedflyer@gmail.com>
2018-03-20 19:42:47 +0100

    del pivy

--- pivy/bld.bat
+++
@@ -1,5 +0,0 @@
-SET GENERATOR=Visual Studio 14 2015 Win64
-
-
-%PYTHON% setup.py clean
-%PYTHON% setup.py install
--- pivy/build_all.sh
+++
@@ -1,3 +0,0 @@
-conda build . --python=2.7
-conda build . --python=3.5
-conda build . --python=3.6

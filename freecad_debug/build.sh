c9dfa9d3f14e069c8ea76c65d2e9cccc0d58f1ca
looooo <sppedflyer@gmail.com>
2018-03-29 11:18:09 +0200

    external smesh

--- freecad_debug/build.sh
+++ freecad_debug/build.sh
@@ -1,11 +1,12 @@
 FREECAD_SOURCE=~/projects/FreeCAD # this is the path to the FreeCAD source (modyfy to your needs)
 
 
-cmake -D CMAKE_BUILD_TYPE=Debug \
+cmake -D CMAKE_BUILD_TYPE=Release \
       -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D CMAKE_PREFIX_PATH=$PREFIX \
       -D CMAKE_C_COMPILER=/usr/bin/gcc-4.8 \
       -D CMAKE_CXX_COMPILER=/usr/bin/g++-4.8 \
+      -D CMAKE_LIBRARY_PATH=$PREFIX/lib \
       -D BUILD_QT5=ON \
       -D NETGENDATA=$PREFIX/include/netgen \
       -D NETGEN_INCLUDEDIR=$PREFIX/include/netgen \
@@ -21,6 +22,9 @@ cmake -D CMAKE_BUILD_TYPE=Debug \
       -D BUILD_FEM_NETGEN=YES \
       -D USE_BOOST_PYTHON=NO \
       -D FREECAD_USE_PYBIND11=YES \
+      -D BUILD_ENABLE_CXX11=ON \
+      -D FREECAD_USE_EXTERNAL_SMESH=ON \
+      -D SMESH_INCLUDE_DIR=$PREFIX/include/smesh \
       ${FREECAD_SOURCE}
 
 make -j${CPU_COUNT} 2>&1 | tee output.txt

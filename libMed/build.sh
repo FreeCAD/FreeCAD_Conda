4663a11b7a80fba3d83247181eeb609af852365c
looooo <sppedflyer@gmail.com>
2018-04-03 12:03:03 +0200

    lib-med: unix:  use ninja

--- libMed/build.sh
+++ libMed/build.sh
@@ -1,13 +1,13 @@
 mkdir build -p
 cd build 
 
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
+cmake -G "Ninja"  \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DINSTALL_DIR=$PREFIX \
       -DMEDFILE_INSTALL_DOC=OFF \
       -DCMAKE_BUILD_TYPE=Release \
       ..
 
-make -j${CPU_COUNT}
-make install
+ninja install
 
 rm -r $PREFIX/share/doc

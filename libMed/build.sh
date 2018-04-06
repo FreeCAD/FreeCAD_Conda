c868bb4223fb809d92f0f4ec7872dfbae8331549
looooo <sppedflyer@gmail.com>
2018-04-06 14:03:26 +0200

    libmed

--- libMed/build.sh
+++ libMed/build.sh
@@ -2,12 +2,10 @@ mkdir build -p
 cd build 
 
 cmake -G "Ninja"  \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DINSTALL_DIR=$PREFIX \
-      -DMEDFILE_INSTALL_DOC=OFF \
-      -DCMAKE_BUILD_TYPE=Release \
+      -D CMAKE_BUILD_TYPE=Release \
+      -D CMAKE_INSTALL_PREFIX=$PREFIX \
+      -D INSTALL_DIR=$PREFIX \
+      -D MEDFILE_INSTALL_DOC=OFF \
       ..
 
 ninja install
-
-rm -r $PREFIX/share/doc

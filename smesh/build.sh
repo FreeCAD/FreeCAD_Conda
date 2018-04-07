8bd888ac3d270eea1f1582c85e22cf8eb55ae35f
looooo <sppedflyer@gmail.com>
2018-04-07 22:22:55 +0200

    update smesh

--- smesh/build.sh
+++ smesh/build.sh
@@ -1,10 +1,13 @@
 mkdir -p build
 cd build
 
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DCMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
+cmake -G "Ninja" \
+	  -D CMAKE_INSTALL_PREFIX=$PREFIX \
+      -D CMAKE_BUILD_TYPE=Release \
+      -D CMAKE_PREFIX_PATH=$PREFIX \
+      -D CMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
+      -D CMAKE_CXX_FLAGS="-std=c++11" \
+      -D ENABLE_MED=ON \
       ~/projects/SMESH
 
-make -j${CPU_COUNT} install
+make install

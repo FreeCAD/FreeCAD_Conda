5b57a818f5874733fa7563eb7e346bdabe68291e
looooo <sppedflyer@gmail.com>
2018-06-12 09:43:15 +0200

    udate freecad-debug to cb3

--- freecad_debug/build.sh
+++ freecad_debug/build.sh
@@ -1,31 +1,46 @@
 FREECAD_SOURCE=~/projects/FreeCAD # this is the path to the FreeCAD source (modyfy to your needs)
 
+declare -a CMAKE_PLATFORM_FLAGS
+if [ `uname` = "Darwin" ]; then
 
-cmake -D CMAKE_BUILD_TYPE=Release \
-      -D CMAKE_INSTALL_PREFIX=$PREFIX \
-      -D CMAKE_PREFIX_PATH=$PREFIX \
+      sed -i '' 's/Xcode-9.app/Xcode.app/g' $PREFIX/lib/cmake/opencascade/OpenCASCADEVisualizationTargets.cmake
+      CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
+      SOME_VAR="-D BUILD_WEB:BOOL=OFF \
+                -D BUILD_START:BOOL=OFF \
+                -D BUILD_FEM_NETGEN=OFF \
+                -D OCCT_CMAKE_FALLBACK:BOOL=OFF \
+             "
+else
+      SOME_VAR="-D BUILD_WEB:BOOL=ON \
+                -D BUILD_FEM_NETGEN:BOOL=ON \
+                -D OCCT_CMAKE_FALLBACK:BOOL=OFF \
+             "
+fi
+
+export LIBRARY_PATH=$PREFIX/lib
+
+cmake -G "Ninja" \
       -D CMAKE_C_COMPILER=/usr/bin/gcc-4.8 \
       -D CMAKE_CXX_COMPILER=/usr/bin/g++-4.8 \
-      -D CMAKE_LIBRARY_PATH=$PREFIX/lib \
-      -D BUILD_QT5=ON \
-      -D NETGENDATA=$PREFIX/include/netgen \
-      -D NETGEN_INCLUDEDIR=$PREFIX/include/netgen \
-      -D NGLIB_INCLUDE_DIR=$PREFIX/include/nglib \
-      -D OCC_INCLUDE_DIR=$PREFIX/include/opencascade \
-      -D OCC_LIBRARY_DIR=$PREFIX/lib \
-      -D OCC_LIBRARIES=$PREFIX/lib CACHE PATH \
+      -D BUID_WITH_CONDA:BOOL=ON \
+      -D CMAKE_BUILD_TYPE=Release \
+      -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
+      -D CMAKE_PREFIX_PATH:FILEPATH=$PREFIX \
+      -D CMAKE_LIBRARY_PATH:FILEPATH=$PREFIX/lib \
+      -D CMAKE_INCLUDE_PATH:FILEPATH=$PREFIX/include \
+      -D BUILD_QT5:BOOL=ON \
       -D FREECAD_USE_OCC_VARIANT="Official Version" \
-      -D OCC_OCAF_LIBRARIES=$PREFIX/lib CACHE PATH \
-      -D SWIG_DIR=$PREFIX/share/swig/3.0.8 \
-      -D SWIG_EXECUTABLE=$PREFIX/bin/swig \
-      -D PYTHON_EXECUTABLE=$PYTHON \
-      -D BUILD_FEM_NETGEN=YES \
-      -D USE_BOOST_PYTHON=NO \
-      -D FREECAD_USE_PYBIND11=YES \
-      -D BUILD_ENABLE_CXX11=ON \
-      -D SMESH_INCLUDE_DIR=$PREFIX/include/smesh \
+      -D OCC_INCLUDE_DIR:FILEPATH=$PREFIX/include \
+      -D USE_BOOST_PYTHON:BOOL=OFF \
+      -D FREECAD_USE_PYBIND11:BOOL=ON \
+      -D BUILD_ENABLE_CXX11:BOOL=ON \
+      -D SMESH_INCLUDE_DIR:FILEPATH=$PREFIX/include/smesh \
       -D FREECAD_USE_EXTERNAL_SMESH=ON \
-      -D BUILD_FLAT_MESH=ON \
+      -D BUILD_FLAT_MESH:BOOL=ON \
+      -D BUILD_WITH_CONDA:BOOL=ON \
+      -D PYTHON_EXECUTABLE:FILEPATH=$PREFIX/bin/python \
+      ${SOME_VAR} \
+      ${CMAKE_PLATFORM_FLAGS[@]} \
       ${FREECAD_SOURCE}
 
 make -j${CPU_COUNT} 2>&1 | tee output.txt

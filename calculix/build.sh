e555df22eb08b7e286bd123175c1975eba94e120
looooo <sppedflyer@gmail.com>
2018-09-22 09:16:20 +0200

    calculix: use external deps (spooles, arpack) gmsh: now included in conda-forge

--- calculix/build.sh
+++ calculix/build.sh
@@ -1,14 +1,4 @@
-
-cd ARPACK
-sh ./fixhome.sh
-make lib
-
-cd ..
-cd SPOOLES.2.2
-make lib -f makefile_MT
-
-cd ..
-cd CalculiX/ccx_2.12/src;
+cd ccx*/src
 make -f Makefile_MT
 
 cp ccx_2.12_MT $PREFIX/bin/ccx

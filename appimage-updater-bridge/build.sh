051f56232d4150735b24c87c0a27a896295f8fda
looooo <sppedflyer@gmail.com>
2020-04-07 20:46:30 +0200

    update appimage-updater-recipe

--- appimage-updater-bridge/build.sh
+++ appimage-updater-bridge/build.sh
@@ -4,7 +4,8 @@ cd build
 cmake -G "Ninja" \
       -D CMAKE_BUILD_TYPE=Release \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
-      -D CMAKE_INSTALL_LIBDIR:FILEPATH=$PREFIX/lib \
+      -D CMAKE_INSTALL_LIBDIR:FILEPATH=$PREFIX/plugins \
+      -D BUILD_AS_PLUGIN:BOOL=ON \
       ..
 
 ninja install
